704f8a2dd4b74b8f6f2a2b713a2fcf0d0252d31c
sreehithaR <rangapurams_ug_22@ece.nits.ac.in>
2026-03-29 19:09:30 +0530

    fix indentation

--- utils/rag.py
+++ utils/rag.py
@@ -1,12 +1,11 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.chains import RetrievalQA
 
 from transformers import pipeline
 from langchain_community.llms import HuggingFacePipeline
+from langchain.chains import RetrievalQA
 
 
 def process_docs(file_path):
@@ -16,17 +15,13 @@ def process_docs(file_path):
     splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
     texts = splitter.split_documents(documents)
 
-    # ✅ FREE embeddings (no OpenAI)
     embeddings = HuggingFaceEmbeddings()
-
     db = FAISS.from_documents(texts, embeddings)
 
     return db
 
 
 def ask_question(db, query):
-    docs = db.similarity_search(query)
-
     pipe = pipeline(
         "text2text-generation",
         model="google/flan-t5-small",
@@ -35,10 +30,10 @@ def ask_question(db, query):
 
     llm = HuggingFacePipeline(pipeline=pipe)
 
-   qa = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type="stuff",
-    retriever=db.as_retriever()
-)
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=db.as_retriever()
+    )
 
-return qa.run(query)
+    return qa.run(query)
